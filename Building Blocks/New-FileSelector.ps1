59dc75d0202554312576872c88a8a38d4f1b3439
mturbes <michael.turbes@gmail.com>
2019-09-09 20:17:41 -0500

    Create New-FileSelector.ps1

---
+++ Building Blocks/New-FileSelector.ps1
@@ -0,0 +1,14 @@
+﻿function New-FileSelector
+{
+  Add-Type -AssemblyName System.Windows.Forms
+  
+  $Prompt = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = $PSScriptRoot
+    Filter = 'PowerShell Scripts (*.ps1)|*.ps1|PowerShell Modules (*.psm1)|*.psm1'
+  }
+  $null = $Prompt.ShowDialog()
+  
+  $prompt.FileName
+}
+
+
